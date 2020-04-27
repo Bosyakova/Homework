0bc9bd4c6fe8ed7d3088a3687e487edb03bd19b3
Катюша <katyabos96@icloud.com>
2020-04-27 16:47:40 +0200

    файл менеджер

--- src/main/java/Com/Home/FileManager/FileGid.java
+++ src/main/java/Com/Home/FileManager/FileGid.java
@@ -4,107 +4,89 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.Scanner;
 
-    public class FileGid {
-        private StringBuilder currentPath = new StringBuilder();
-        private Scanner scanner = new Scanner(System.in);
+public class FileGid {
+    private StringBuilder currentPath;
+    private Scanner scanner = new Scanner(System.in);
 
-        public File pave() {
-            while (true) {
-                try {
-                    choiceRoot();
-                    printCurFileContent();
-                    run();
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
-                    currentPath = new StringBuilder();
-                }
-            }
-        }
-
-        private File run() {
-            while (true) {
-                String input = scanner.nextLine();
-                implInput(input);
+    public File pave() {
+        while (true) {
+            try {
+                chooseRoot();
                 printCurFileContent();
-                deleteFile(input);
+                run();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                currentPath = new StringBuilder();
             }
         }
+    }
 
-        private void implInput(String input) {
-            checkExit(input);
-            checkBackToChoiceRoot(input);
-            input = input.trim();
-            if (input.startsWith("cd ")) {
-                input = input.substring(3);
-                input = input.trim();
-                String[] pathParts = input.split("\\\\");
-                for (String path : pathParts) {
-                    if (path.equals("..")) {
-                        int slashIndex = currentPath.lastIndexOf("\\");
-                        if (slashIndex != -1) {
-                            currentPath.delete(slashIndex - 1, currentPath.length());
-                            slashIndex = currentPath.lastIndexOf("\\");
-                            currentPath.delete(slashIndex + 1, currentPath.length());
-                        } else {
-                            choiceRoot();
-                        }
-                    } else {
-                        currentPath.append(path).append("\\");
-                    }
+    private File run() {
+        while (true) {
+            String input = scanner.nextLine();
+            implInput(input);
+        }
+    }
+
+
+    private void implInput(String input) {
+        input = input.trim();
+        if (input.startsWith("cd ")) {
+            input = input.substring(3);
+            chooseDestination(input);
+        }
+    }
+
+    private void chooseDestination(String input) {
+        input = input.trim();
+        String[] pathParts = input.split("\\\\");
+        for (String path : pathParts) {
+            if (path.equals("..")) {
+                if (currentPath.indexOf("\\") != currentPath.lastIndexOf("\\")) {
+                    String newPath = currentPath.toString().replaceAll("\\\\[a-zA-Z0-9]*\\\\$", "\\\\");
+                    currentPath = new StringBuilder(newPath);
+                    printCurFileContent();
+                } else {
+                    chooseRoot();
                 }
+            } else {
+                currentPath.append(path).append("\\");
+                printCurFileContent();
             }
         }
+    }
 
-        private void deleteFile(String input) {
-            checkExit(input);
-            checkBackToChoiceRoot(input);
-            input = input.trim();
-            if (input.startsWith("del ")) {
-                input = input.substring(4);
-                input = input.trim();
-                File file = new File(currentPath.toString());
-                File[] files = file.listFiles();
-                if (files != null) {
-                    for (File innerFile : files) {
-                        String name = innerFile.getName();
-                        if (input.equals(name)) {
-                            innerFile.delete();
-                            System.out.println(name + " was deleted");
-                        }
-                    }
-                }
-            }
-        }
 
-        private void choiceRoot() {
-            File[] roots = File.listRoots();
-            System.out.println(Arrays.toString(roots));
-            String input = scanner.nextLine().toUpperCase();
-            checkExit(input);
+    private void chooseRoot() {
+        currentPath = new StringBuilder();
+        File[] roots = File.listRoots();
+        System.out.println(Arrays.toString(roots));
+        while (currentPath.length() == 0) {
+            String input = scanner.nextLine();
             for (File file : roots) {
                 String path = file.getPath();
-                if (path.startsWith(input)) {
+                if (path.startsWith(input) || path.equalsIgnoreCase(input) || (path).equalsIgnoreCase(input + ":\\")) {
                     currentPath.append(path);
                     return;
                 }
             }
         }
+    }
 
-        private void printCurFileContent() {
-            File file = new File(currentPath.toString());
-            if (!file.exists()) {
-                throw new IllegalStateException("The file doesn't exist");
-            }
-            File[] files = file.listFiles();
-            if (files != null) {
-                if (files.length == 0) {
-                    System.out.println("Directory: " + file.getName() + " is empty");
-                }
-                for (File innerFile : files) {
-                    System.out.println(innerFile);
-                }
-            }
+    private void printCurFileContent() {
+        File file = new File(currentPath.toString());
+        if (!file.exists()) {
+            throw new IllegalStateException("File do not exists");
         }
+        File[] files = file.listFiles();
+        if (files != null) {
+            for (File innerFile : files) {
+                System.out.println(innerFile);
+            }
+        } else {
+            System.out.println("Directory is empty");
+        }
+    }
 
         private void checkExit(String input) {
             String s = "exit";
